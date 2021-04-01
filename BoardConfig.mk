be29f04fa34ce69fb981bb4c72de68e60eb3b10d
michie <tha_michie@hotmail.com>
2021-04-01 21:24:05 +0200

    dragon: cleanup: Device config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The Android Open-Source Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,45 +14,71 @@
 # limitations under the License.
 #
 
-$(shell mkdir -p out/target/product/dragon/root/vendor/firmware)
+BUILD_BROKEN_DUP_RULES := true
 
-PRODUCT_SOONG_NAMESPACES += external/mesa3d
-
-# Build a separate vendor.img
-TARGET_COPY_OUT_VENDOR := vendor
-
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+# Architecture
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
+TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a53
-TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
-BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_SOURCE := kernel/google/tegra
-TARGET_KERNEL_CONFIG := lineageos_dragon_defconfig
+# Board
+TARGET_BOARD_PLATFORM := tegra
+TARGET_NO_BOOTLOADER  := true
+
+BOARD_FLASH_BLOCK_SIZE             := 4096
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 26738688
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 26767360
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 419430400
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 10099646976
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 3758096384 
+BOARD_VENDORIMAGE_PARTITION_SIZE   := 268435456
+TARGET_USERIMAGES_USE_EXT4         := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE  := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR             := vendor
+
+# Bootloader versions
+TARGET_BOARD_INFO_FILE := device/google/dragon/board-info.txt
+
+# Manifest
+DEVICE_MANIFEST_FILE := device/google/dragon/hidl/manifest.xml
+DEVICE_MATRIX_FILE   := device/google/dragon/hidl/compatibility_matrix.xml
+
+# Kernel
+BOARD_KERNEL_BASE           := 0x10000000
+BOARD_KERNEL_PAGESIZE       := 2048
+KERNEL_TOOLCHAIN            := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-6.4.1/bin
+KERNEL_TOOLCHAIN_PREFIX     := aarch64-linux-gnu-
+TARGET_KERNEL_SOURCE        := kernel/google/tegra
+TARGET_KERNEL_CONFIG        := lineageos_dragon_defconfig
 TARGET_LINUX_KERNEL_VERSION := 3.18
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-BOARD_KERNEL_IMAGE_NAME := Image.fit
+TARGET_KERNEL_ARCH          := arm64
+TARGET_KERNEL_HEADER_ARCH   := arm64
+BOARD_KERNEL_IMAGE_NAME     := Image.fit
 
-# Disable emulator for "make dist" until there is a 64-bit qemu kernel
-BUILD_EMULATOR := false
+# Audio
+BOARD_SUPPORT_NVOICE      := true
+BOARD_SUPPORT_NVAUDIOFX   := true 
+BOARD_USES_GENERIC_AUDIO  := false
+BOARD_USES_ALSA_AUDIO     := true
+USE_XML_AUDIO_POLICY_CONF := 1
 
-TARGET_BOARD_PLATFORM := tegra210_dragon
-TARGET_BOARD_INFO_FILE ?= device/google/dragon/board-info.txt
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_CUSTOM_BT_CONFIG := device/google/dragon/bluetooth/vnd_dragon.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/google/dragon/bluetooth
 
-TARGET_BOOTLOADER_BOARD_NAME := dragon
-TARGET_RELEASETOOLS_EXTENSIONS := device/google/dragon
-
+# Graphics
 USE_OPENGL_RENDERER := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 BOARD_DISABLE_TRIPLE_BUFFERED_DISPLAY_SURFACES := true
@@ -60,96 +86,39 @@ BOARD_USES_DRM_HWCOMPOSER := true
 BOARD_DRM_HWCOMPOSER_BUFFER_IMPORTER := nvidia-gralloc
 BOARD_USES_LIBDRM := true
 TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
-#TARGET_USES_HWC2 := true
 TARGET_FORCES_DRM_HWC1 := true
 
 PRESENT_TIME_OFFSET_FROM_VSYNC_NS := 0
 VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USES_MKE2FS := true
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3758096384
-BOARD_CACHEIMAGE_PARTITION_SIZE := 419430400
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_PARTITION_SIZE := 268435456
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_FLASH_BLOCK_SIZE := 4096
-
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
-
-BUILD_BROKEN_DUP_RULES := true
-
-BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_USES_GENERIC_INVENSENSE := false
-
-BOARD_USES_GENERIC_AUDIO := false
-BOARD_USES_ALSA_AUDIO := true
-
-BOARD_VENDOR_USE_SENSOR_HAL := sensor_hub
-
-BOARD_WIDEVINE_OEMCRYPTO_LEVEL := 1
-
+# Odm permissions
 TARGET_FS_CONFIG_GEN += device/google/dragon/config.fs
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/google/dragon/bluetooth
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_CUSTOM_BT_CONFIG := device/google/dragon/bluetooth/vnd_dragon.txt
-
-# Wifi related defines
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-WPA_SUPPLICANT_VERSION      := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER        := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
-BOARD_WLAN_DEVICE           := bcmdhd
-WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
-WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
-WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
-
-# Enable dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),userdebug)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-
-ART_USE_HSPACE_COMPACT=true
+# Recovery
+TARGET_RECOVERY_FSTAB := device/google/dragon/fstab.dragon
 
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2019-06-05
 
+# Sepolicy
 BOARD_SEPOLICY_DIRS += device/google/dragon/sepolicy
-
-# add firmware update to the updater binary
-TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_dragon
-TARGET_RECOVERY_UPDATER_EXTRA_LIBS +=
-#TARGET_RECOVERY_UI_LIB := librecovery_ui_dragon
-
-ifeq ($(SECURE_OS_BUILD),tlk)
-  BOARD_SUPPORT_ROLLBACK_PROTECTION := true
-endif
-
-# Enable real time lockscreen charging current values
-BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
-
-#BOARD_HAL_STATIC_LIBRARIES := libhealthd.dragon
-
-WITH_LINEAGE_CHARGER := false
 
 # Shims
 TARGET_LD_SHIM_LIBS += \
     /vendor/lib/hw/camera.dragon.so|libshim_camera.so
 
-# Testing related defines
-BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/dragon-setup.sh
-
-# Vendor Interface Manifest
-DEVICE_MANIFEST_FILE := device/google/dragon/hidl/manifest.xml
-DEVICE_MATRIX_FILE := device/google/dragon/hidl/compatibility_matrix.xml
-
-TARGET_FLATTEN_APEX := true
+# Treble
+PRODUCT_VENDOR_MOVE_ENABLED := true
+	
+# Wifi
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE                := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The Android Open-Source Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,52 +14,40 @@
 # limitations under the License.
 #
 
-# There are three modes for TLK components, controlled by SECURE_OS_BUILD:
-#
-# "SECURE_OS_BUILD = tlk" will build everything from source.
-# "SECURE_OS_BUILD = client_only" will build TLK client components from
-#                    source but use a prebuilt version of the secure OS.
-# "SECURE_OS_BUILD = false" will will use prebuilts for TLK and client
-#                    components.
-
-# Device was launched with M
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-$(call inherit-product, device/google/dragon/hidl/hidl.mk)
+$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
-$(call inherit-product, device/google/dragon/permissions.mk)
+$(call inherit-product, build/target/product/vboot.mk)
 
-# By default build TLK from source if it is available, otherwise use
-# prebuilts.  To force using the prebuilt while having the source, set:
-# SECURE_OS_BUILD=false
-ifeq ($(wildcard vendor/nvidia/dragon-tlk/tlk),vendor/nvidia/dragon-tlk/tlk)
-    SECURE_OS_BUILD ?= tlk
-endif
+$(call inherit-product, vendor/google/dragon/device-vendor.mk)
+$(call inherit-product, vendor/google/dragon-common/device-vendor.mk)
 
-# prebuilt kernel config
-#
-#ifeq ($(TARGET_PREBUILT_KERNEL),)
-#LOCAL_KERNEL := device/google/dragon/Image.fit
-#else
-#LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-#endif
-#
-#PRODUCT_COPY_FILES := \
-#    $(LOCAL_KERNEL):kernel \
-#
+# System properties
+include $(LOCAL_PATH)/system_prop.mk
 
-ifeq ($(TARGET_PRODUCT), ryu_kasan)
-LOCAL_FSTAB := $(LOCAL_PATH)/fstab.dragon.nocrypt
-else
-LOCAL_FSTAB := $(LOCAL_PATH)/fstab.dragon
-endif
+PRODUCT_CHARACTERISTICS  := tablet,nosdcard
+PRODUCT_AAPT_CONFIG      := normal large xlarge
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+TARGET_SCREEN_HEIGHT     := 2560
+TARGET_SCREEN_WIDTH      := 1800
 
-TARGET_RECOVERY_FSTAB = $(LOCAL_FSTAB)
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    device/google/dragon/overlay
+	
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/google/dragon \
+    external/mesa3d
 
-PRODUCT_BUILD_RECOVERY_IMAGE := true
-
-PRODUCT_BUILD_BOOT_IMAGE := true
-
+# Init related
+PRODUCT_PACKAGES += \
+    fstab.dragon \
+    init.dragon.rc \
+    init.dragon.usb.rc \
+    init.recovery.dragon.rc
+    
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/dump_bq25892.sh:system/bin/dump_bq25892.sh \
     $(LOCAL_PATH)/touchfwup.sh:system/bin/touchfwup.sh \
@@ -73,33 +61,50 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/speakerdsp.ini:system/etc/cras/speakerdsp.ini \
     $(LOCAL_PATH)/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd.cal \
     $(LOCAL_FSTAB):$(TARGET_COPY_OUT_RAMDISK)/fstab.dragon \
-    $(LOCAL_FSTAB):$(TARGET_COPY_OUT_VENDOR)/etc/fstab.dragon
-
+    $(LOCAL_FSTAB):$(TARGET_COPY_OUT_VENDOR)/etc/fstab.dragon	
+    
+# Key layouts
 PRODUCT_PACKAGES += \
-    libwpa_client \
-    hostapd \
-    wificond \
-    wifilogd \
-    wpa_supplicant \
-    wpa_supplicant.conf \
-    libwifikeystorehal \
-    fs_config_files \
-    fwtool
-
-PRODUCT_COPY_FILES += \
-    device/google/dragon/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    device/google/dragon/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
-
-PRODUCT_COPY_FILES += \
     device/google/dragon/dragon-keypad.kl:system/usr/keylayout/dragon-keypad.kl
 
-ifeq ($(TARGET_BUILD_VARIANT),eng)
+# Media config
 PRODUCT_PACKAGES += \
-    tinyplay \
-    tinycap \
-    tinymix
-endif
-
+    media_codecs.xml \
+    media_codecs_performance.xml \
+    media_profiles_V1_0.xml
+    
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
+    $(LOCAL_PATH)/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/enctune.conf:$(TARGET_COPY_OUT_VENDOR)/etc/enctune.conf \
+    $(LOCAL_PATH)/enctune.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/enctune.conf
+	
+# Audio
+PRODUCT_PACKAGES += \
+    libaudio-resampler \
+    audio.primary.dragon \
+    sound_trigger.primary.dragon \
+    audio.a2dp.default \
+    audio.bluetooth.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    audio_effects.xml \
+    audio_policy_configuration.xml \
+    audio_policy_configuration_bluetooth_legacy_hal.xml \
+    audio_policy_volumes_drc.xml \
+    mixer_paths_0.xml \
+    a2dp_audio_policy_configuration.xml \
+    bluetooth_audio_policy_configuration.xml \
+    usb_audio_policy_configuration.xml \
+    r_submix_audio_policy_configuration.xml \
+    default_volume_tables.xml \
+    audio.bluetooth.default
+    
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
@@ -113,230 +118,21 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
 
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
-    $(LOCAL_PATH)/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    $(LOCAL_PATH)/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bluetooth/BCM4354_003.001.012.0443.0863.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4350c0.hcd
-
-# Bluetooth HAL
+# Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor
-
-# Copy dsp firmware to the vendor parition so it is available when hotwording
-# starts
+    
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rt5677_elf_vad:vendor/firmware/rt5677_elf_vad
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/enctune.conf:system/etc/enctune.conf
-
-PRODUCT_AAPT_CONFIG := normal large xlarge hdpi xhdpi xxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-PRODUCT_CHARACTERISTICS := tablet,nosdcard
-
-DEVICE_PACKAGE_OVERLAYS := \
-    $(LOCAL_PATH)/overlay
-
-PRODUCT_TAGS += dalvik.gc.type-precise
-
-PRODUCT_PACKAGES += \
-    librs_jni \
-    com.android.future.usb.accessory
-
-#TODO(dgreid) is this right?
-PRODUCT_PROPERTY_OVERRIDES := \
-    wifi.interface=wlan0 \
-    wifi.direct.interface=p2p-dev-wlan0 \
-    ro.hwui.texture_cache_size=86 \
-    ro.hwui.layer_cache_size=56 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=40 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024 \
-    ro.hwui.disable_scissor_opt=true \
-    ro.recents.grid=true
-
-# mobile data provision prop
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.android.prov_mobiledata=false
-
-# facelock props
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.facelock.black_timeout=700 \
-    ro.facelock.det_timeout=2500 \
-    ro.facelock.rec_timeout=3500 \
-    ro.facelock.est_max_time=500
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.media_vol_steps=25
-
-# Cast
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.wfd.enable=1
-
-# The default locale should be determined from VPD, not from build.prop.
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
-    ro.product.locale
-
-# OEM Unlock reporting
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.oem_unlock_supported=1
-
-# setup dalvik vm configs.
-$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
-
-# set default USB configuration
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=320 \
-    ro.opengles.version=196610
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.sys.usb.config=adb \
-    ro.adb.secure=0 \
-    service.adb.root=1
-
-# Update the recovery image only if the option is enabled
-# under Developer options
-# by default, do not update the recovery image
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.recovery_update=false
-
-# for audio
-#TODO(dgreid) do we need libnvvisualizer?
-PRODUCT_PACKAGES += \
-    audio.primary.dragon \
-    sound_trigger.primary.dragon \
-    audio.a2dp.default \
-    audio.bluetooth.default \
-    audio.usb.default \
-    audio.r_submix.default
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.audio.monitorRotation=true \
-    ro.frp.pst=/dev/block/platform/700b0600.sdhci/by-name/PST
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bt.bdaddr_path=/sys/devices/700b0200.sdhci/mmc_host/mmc0/mmc0:0001/mmc0:0001:2/net/wlan0/address
-
-# Charger
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.charger.enable_suspend=true
-
-# ro.product.first_api_level indicates the first api level the device has commercially launched on.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.first_api_level=23
-
-# for keyboard key mappings
-PRODUCT_PACKAGES += \
-    DragonKeyboard
-
-# Wi-Fi country code setting
-# PRODUCT_PACKAGES += \
-#    DragonParts
-
-# DRM Mappings
-PRODUCT_PROPERTY_OVERRIDES += \
-    camera.flash_off=0 \
-    drm.service.enabled=true \
-    ro.com.widevine.cachesize=16777216
-
-# P2P0 Concurrency
-PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.direct.non-concurrent=true
-
-# Face Unlock
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full
-
-# Google Assistant
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opa.eligible_device=true
-
-# UI
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_hwc_vds=1 \
-    debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=1 \
-    debug.egl.hw=1 \
-    debug.gralloc.enable_fb_ubwc=1 \
-    dalvik.vm.heapsize=36m \
-    dalvik.vm.dex2oat64.enabled=true \
-    dev.pm.dyn_samplingrate=1 \
-    persist.demo.hdmirotationlock=false \
-    debug.sf.recomputecrop=0 \
-    sys.use_fifo_ui=1 \
-    debug.sf.enable_gl_backpressure=1
-
-# HIDL
-PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
-
-# Allows healthd to boot directly from charger mode rather than initiating a reboot.
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.enable_boot_charger_mode=1
-
-# TODO(dgreid) - Add back verity dependencies like flounder has.
-
-$(call inherit-product, build/target/product/vboot.mk)
-
-# only include verity on user builds
-ifeq ($(TARGET_BUILD_VARIANT),user)
-$(call inherit-product, build/target/product/verity.mk)
-# including verity.mk automatically enabled boot signer which conflicts with
-# vboot
-PRODUCT_SUPPORTS_BOOT_SIGNER := false
-PRODUCT_SUPPORTS_VERITY_FEC := false
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/700b0600.sdhci/by-name/APP
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/700b0600.sdhci/by-name/VNR
-endif
-
-# The following group is necessary to support building the NVIDIA vendor
-# HALs and prebuilts.
-BOARD_SUPPORT_NVOICE := true
-BOARD_SUPPORT_NVAUDIOFX :=true
+    $(LOCAL_PATH)/bluetooth/BCM4354_003.001.012.0443.0863.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4350c0.hcd       
+ 
+# Graphics
+BOARD_GPU_DRIVERS := tegra
 BOARD_USES_LIBDRM := true
+ENABLE_LIBDRM := true
 NVRM_GPU_SUPPORT_NOUVEAU := 1
 NV_GPU_USE_SYNC_FD := 1
 USE_DRM_HWCOMPOSER := 1
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.vulkan=tegra
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.stagefright.ccodec=0
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lmk.low=1001 \
-    ro.lmk.medium=800 \
-    ro.lmk.critical=0 \
-    ro.lmk.critical_upgrade=false \
-    ro.lmk.upgrade_pressure=100 \
-    ro.lmk.downgrade_pressure=100 \
-    ro.lmk.kill_heaviest_task=true \
-    ro.lmk.kill_timeout_ms=100 \
-    ro.lmk.use_minfree_levels=true
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bionic.ld.warning=0
-
-$(call inherit-product-if-exists, hardware/nvidia/tegra132/tegra132.mk)
-$(call inherit-product-if-exists, vendor/google/dragon/device-vendor.mk)
-$(call inherit-product-if-exists, vendor/google/dragon-common/device-vendor.mk)
-$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
-
-ENABLE_LIBDRM := true
-BOARD_GPU_DRIVERS := tegra
-USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_PACKAGES += \
     f54test \
     libvulkan \
@@ -345,12 +141,32 @@ PRODUCT_PACKAGES += \
     rmi4update \
     rmihidtool
 
+# HIDL
+PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
+
+include $(LOCAL_PATH)/hidl/hidl.mk
+
+# Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite-vendorcompat
 
+# USB accessory 
+PRODUCT_PACKAGES += \
+    librs_jni \
+    com.android.future.usb.accessory    
+   
 # Vendor seccomp policy files for media components:
 PRODUCT_COPY_FILES += \
     device/google/dragon/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     device/google/dragon/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
-$(call inherit-product-if-exists, vendor/nvidia/dragon/dragon-vendor.mk)
+# WiFi
+PRODUCT_PACKAGES += \
+    hostapd \
+    wificond \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf \
+    p2p_supplicant_overlay.conf \
+    wpa_supplicant_overlay.conf \
+    wifi_scan_config.conf
